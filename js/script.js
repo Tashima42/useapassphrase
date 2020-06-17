1d08d9d3ec894e630676692bbb0cf187dd338fe8
Pedro Tashima <pobis31@gmail.com>
2020-06-16 22:12:06 -0300

    função removida

--- js/script.js
+++ js/script.js
@@ -28,38 +28,6 @@ function setStyleFromWordNumber(passwordField, numberOfWords) {
   passwordField.setAttribute('style', 'font-size: ' + newSize + 'px;');
 }
 
-function convertSecondsToReadable(seconds) {
-  var timeString = '';
-  var crackabilityColor = 'green';
-
-  // Enumerate all the numbers
-  var numMilliseconds = seconds * 1000;
-  var numSeconds     = Math.floor(seconds);
-  var numMinutes     = Math.floor(numSeconds / 60);
-  var numHours       = Math.floor(numSeconds / (60 * 60));
-  var numDays        = Math.floor(numSeconds / (60 * 60 * 24));
-  var numYears       = Math.floor(numSeconds / (60 * 60 * 24 * 365));
-  var numCenturies   = Math.floor(numSeconds / (60 * 60 * 24 * 365 * 100));
-
-  if (numMilliseconds < 1000) {
-    timeString = numMilliseconds + ' milliseconds';
-  } else if (numSeconds < 60) {
-    timeString = numSeconds + ' seconds';
-  } else if (numMinutes < 60) {
-    timeString = numMinutes + ' minutes';
-  } else if (numHours < 24) {
-    timeString = numHours + ' hours';
-  } else if (numDays < 365) {
-    timeString = numDays + ' days';
-  } else if (numYears < 100) {
-    timeString = numYears + ' years';
-  } else {
-    timeString = numCenturies + ' centuries';
-  }
-
-  return timeString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
 var selectField = document.getElementById('passphrase_select');
 var passwordField = document.getElementById('passphrase');
 var button = document.querySelector('.btn-generate');
